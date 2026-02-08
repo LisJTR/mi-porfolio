9e26e014d163ecbec4690dc70c104ddd815ae918
Lis Torres <spelive16@gmail.com>
2026-02-08 22:16:52 +0100

    feat(ui): corrección de estilos

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-function toggleImagen() {
-  const imagen = document.getElementById("miImagen");
+function toggleImagen(icono) {
+  const contenedor = icono.parentElement;
+  const imagen = contenedor.querySelector(".imagen-certificado");
+
   imagen.classList.toggle("visible");
 }
 
-// Ocultar al hacer clic fuera
+// cerrar al hacer click fuera
 document.addEventListener("click", function (event) {
-  const contenedor = document.getElementById("contenedorCertificado");
-  const imagen = document.getElementById("miImagen");
-
-  if (!contenedor.contains(event.target)) {
-    imagen.classList.remove("visible");
-  }
+  document.querySelectorAll(".contenedor-certificado").forEach(contenedor => {
+    if (!contenedor.contains(event.target)) {
+      contenedor
+        .querySelector(".imagen-certificado")
+        .classList.remove("visible");
+    }
+  });
 });
 
 
